34472599b99bae87a93e00d619f61296d3299446
angelynaliem <angelynaliem@gmail.com>
2020-11-03 23:37:59 -0500

    logger working

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code away!
+
+//npm install and then create index.js
+
+const server = require("./server.js")
+
+const port = 5000
+
+server.listen(port, () => {
+    console.log(`Server listening on http://localhost:${port}`)
+})
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
+//add cors
+const cors = require("cors")
+
 const server = express();
+
+const userRouter = require("./users/userRouter.js")
+
+server.use(express.json())
+server.use(cors())
+
+server.use("/api/users", userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
