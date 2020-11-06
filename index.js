aef42e3cb023c9299acbea2321018062625bf475
angelynaliem <angelynaliem@gmail.com>
2020-11-06 00:01:46 -0500

    installed dotenv, added port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // code away!
 
 //npm install and then create index.js
+require("dotenv").config()
 
 const server = require("./server.js")
 
-const port = 5000
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`)
